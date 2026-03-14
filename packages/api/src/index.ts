8e6b89b79a6b2ddfe98a83676f46dbd44e8ce30b
raashed <mohammedraashed10@gmail.com>
2026-03-14 20:33:08 +0530

    refactor(trpc): added tRPC context for managing /api

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,1 +1,73 @@
-console.log("Hello via Bun!");
+import express from "express";
+import jwt from "jsonwebtoken";
+import { router } from "@/src/trpc.js";
+import { TRPCError } from "@trpc/server";
+import { JWT_SECRET } from "@todo-list/config";
+import { userRouter } from "@/src/routes/v1/user.js";
+import { todoRouter } from "@/src/routes/v1/todo.js";
+import * as trpcExpress from "@trpc/server/adapters/express";
+
+export const appRouter = router({
+  v1: router({
+    user: userRouter,
+    todo: todoRouter,
+  }),
+});
+
+export type AppRouter = typeof appRouter;
+
+export const app = express();
+app.use(express.json());
+
+app.use(
+  "/trpc",
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+    createContext({ req }: trpcExpress.CreateExpressContextOptions) {
+      const authHeader: string | undefined = req.headers.authorization;
+
+      if (!authHeader || authHeader.endsWith(" Bearer")) {
+        return {
+          userId: undefined,
+        };
+      }
+
+      const token = authHeader.split(" ")[1];
+
+      if (!token) {
+        return {
+          userId: undefined,
+        };
+      }
+
+      if (!JWT_SECRET) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "the JWT_SECRET went missing",
+        });
+      }
+
+      try {
+        const decoded = jwt.verify(token, JWT_SECRET);
+
+        if (
+          typeof decoded !== "object" ||
+          typeof decoded.userId !== "string" ||
+          decoded === null
+        ) {
+          return {
+            userId: undefined,
+          };
+        }
+
+        return {
+          userId: decoded.userId,
+        };
+      } catch {
+        return {
+          userId: undefined,
+        };
+      }
+    },
+  }),
+);
