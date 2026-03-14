4f5722bfa069cd8741cea6e8c2d5d9034cc39271
raashed <mohammedraashed10@gmail.com>
2026-03-14 15:07:05 +0530

    feat(utils): added cn function using clsx and tailwind-merge

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,1 +1,6 @@
-console.log("Hello via Bun!");
+import clsx, { ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
