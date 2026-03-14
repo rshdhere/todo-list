1ee317c7f52797fb8cf39c4c22a2b2576e35d2a8
raashed <mohammedraashed10@gmail.com>
2026-03-14 20:29:26 +0530

    feat(config): added a config-file for distribution of /api

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -1,1 +1,3 @@
-console.log("Hello via Bun!");
+export const JWT_SECRET = process.env.JWT_SECRET;
+export const SERVER_PORT = process.env.SERVER_PORT;
+export const ENVIRONMENT = process.env.NODE_ENV;
