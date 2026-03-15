950a6b7fd243c7705fef65d9c3352180aa30570f
raashed <mohammedraashed10@gmail.com>
2026-03-15 13:58:31 +0530

    feat(user): completed .signup and .signin tRPC-procedures

--- packages/api/src/routes/v1/user.ts
+++ packages/api/src/routes/v1/user.ts
@@ -1,14 +1,90 @@
-import { publicProcedure, router } from "@/src/trpc.js";
+import jwt from "jsonwebtoken";
+import { TRPCError } from "@trpc/server";
+import { JWT_SECRET } from "@todo-list/config";
+import { db, eq } from "@todo-list/drizzle";
 import { authSchema } from "@todo-list/validators";
+import { publicProcedure, router } from "@/src/trpc.js";
+import { usersTable } from "@todo-list/drizzle/database";
 
 export const userRouter = router({
   signup: publicProcedure
     .input(authSchema.input)
     .output(authSchema.output)
-    .mutation(({ input }) => {}),
+    .mutation(async ({ input }) => {
+      const users = await db
+        .select()
+        .from(usersTable)
+        .where(eq(usersTable.email, input.email));
+
+      if (users.length > 0) {
+        throw new TRPCError({
+          code: "CONFLICT",
+          message: "user already exists",
+        });
+      }
+
+      const HashedPassword = await Bun.password.hash(input.password);
+
+      const [user] = await db
+        .insert(usersTable)
+        .values({
+          email: input.email,
+          password: HashedPassword,
+        })
+        .returning({ userId: usersTable.id });
+
+      if (!user) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "failed to create user",
+        });
+      }
+
+      return { userId: user.userId };
+    }),
 
   signin: publicProcedure
     .input(authSchema.input)
     .output(authSchema.signinOutput)
-    .mutation(({ input }) => {}),
+    .mutation(async ({ input }) => {
+      const [user] = await db
+        .select({
+          userId: usersTable.id,
+          password: usersTable.password,
+        })
+        .from(usersTable)
+        .where(eq(usersTable.email, input.email));
+
+      if (!user) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "user not found",
+        });
+      }
+
+      const isValidPassword = await Bun.password.verify(
+        input.password,
+        user.password,
+      );
+
+      if (!isValidPassword) {
+        throw new TRPCError({
+          code: "UNAUTHORIZED",
+          message: "invalid credentials",
+        });
+      }
+
+      if (!JWT_SECRET) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "JWT secret is not configured",
+        });
+      }
+
+      const token = jwt.sign({ userId: user.userId }, JWT_SECRET, {
+        expiresIn: "1hr",
+      });
+
+      return { token };
+    }),
 });
