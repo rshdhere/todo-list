966900f710062a3fbc9dba81fdd04687a28c3009
raashed <mohammedraashed10@gmail.com>
2026-03-15 15:51:14 +0530

    refactor(bun): using bun as process and migrating .env from here to /apps

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -1,4 +1,4 @@
-export const JWT_SECRET = process.env.JWT_SECRET;
-export const SERVER_PORT = process.env.SERVER_PORT;
-export const ENVIRONMENT = process.env.NODE_ENV;
-export const DATABASE_URL = process.env.DATABASE_URL;
+export const JWT_SECRET = Bun.env.JWT_SECRET;
+export const SERVER_PORT = Number(Bun.env.SERVER_PORT);
+export const ENVIRONMENT = Bun.env.NODE_ENV;
+export const DATABASE_URL = Bun.env.DATABASE_URL;
