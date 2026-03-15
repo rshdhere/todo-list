c33fdbb710af244f5fa9f8b949cee1c69a59e5b5
raashed <mohammedraashed10@gmail.com>
2026-03-15 13:47:44 +0530

    refactor(config): added DATABASE_URL to config to manage /api

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -1,3 +1,4 @@
 export const JWT_SECRET = process.env.JWT_SECRET;
 export const SERVER_PORT = process.env.SERVER_PORT;
 export const ENVIRONMENT = process.env.NODE_ENV;
+export const DATABASE_URL = process.env.DATABASE_URL;
