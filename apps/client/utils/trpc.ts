69d5a08972e071a6e4caf717f99e661e6f0c38ee
raashed <mohammedraashed10@gmail.com>
2026-03-16 21:00:45 +0530

    refactor(utils): updated tRPC as a utility

--- apps/client/utils/trpc.ts
+++ apps/client/utils/trpc.ts
@@ -1,5 +1,12 @@
-import { AppRouter } from "@todo-list/api";
+import type { AppRouter } from "@todo-list/api";
 import { createTRPCContext } from "@trpc/tanstack-react-query";
 
-export const { TRPCProvider, useTRPC, useTRPCClient } =
-  createTRPCContext<AppRouter>();
+type TRPCContext = ReturnType<typeof createTRPCContext<AppRouter>>;
+
+const trpcContext: TRPCContext = createTRPCContext<AppRouter>();
+
+export const TRPCProvider: TRPCContext["TRPCProvider"] =
+  trpcContext.TRPCProvider;
+export const useTRPC: TRPCContext["useTRPC"] = trpcContext.useTRPC;
+export const useTRPCClient: TRPCContext["useTRPCClient"] =
+  trpcContext.useTRPCClient;
