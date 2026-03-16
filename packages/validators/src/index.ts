0418be11a4cdae6b98876870bc78d954af18862b
raashed <mohammedraashed10@gmail.com>
2026-03-16 15:23:52 +0530

    refactor(signup): added schema for tRPC signup procedure for the /client

--- packages/validators/src/index.ts
+++ packages/validators/src/index.ts
@@ -1,2 +1,7 @@
-export * from "./user/validation.js";
-export * from "./todo/validation.js";
+import { z } from "zod";
+import { authSchema } from "@todo-list/validators/user";
+
+export * from "@todo-list/validators/user";
+export * from "@todo-list/validators/todo";
+
+export type SignUpSchema = z.infer<typeof authSchema.input>;
