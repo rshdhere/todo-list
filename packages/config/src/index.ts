c1d60edede45e247c38e6f0ee651cf631be50ee2
raashed <mohammedraashed10@gmail.com>
2026-03-16 20:55:20 +0530

    refactor(config): added /client and /server url's in config-file

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -1,4 +1,6 @@
 export const JWT_SECRET = Bun.env.JWT_SECRET;
-export const SERVER_PORT = Number(Bun.env.SERVER_PORT);
+export const SERVER_PORT = Bun.env.SERVER_PORT;
 export const ENVIRONMENT = Bun.env.NODE_ENV;
 export const DATABASE_URL = Bun.env.DATABASE_URL;
+export const CLIENT_URL = Bun.env.CLIENT_URL;
+export const SERVER_URL = Bun.env.SERVER_URL;
