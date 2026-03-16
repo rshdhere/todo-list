b79059363a6b20bd6f88b534d4f78f7bbe6d6a7b
raashed <mohammedraashed10@gmail.com>
2026-03-16 21:06:19 +0530

    refactor(tRPC): added tRPC useMutation as a backend call to save form data

--- apps/client/app/(auth)/signup/page.tsx
+++ apps/client/app/(auth)/signup/page.tsx
@@ -1,24 +1,30 @@
 "use client";
 
-import { useForm, FieldValues } from "react-hook-form";
+import { useForm } from "react-hook-form";
+import { useMutation } from "@tanstack/react-query";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { authSchema, type SignUpSchema } from "@todo-list/validators";
+import { useTRPC } from "@/utils/trpc";
 
 export default function SignUp() {
+  const trpc = useTRPC();
   const {
     register,
     handleSubmit,
-    formState: { errors, isSubmitting },
+    formState: { errors },
     reset,
   } = useForm<SignUpSchema>({
     resolver: zodResolver(authSchema.input),
   });
 
-  const onSubmit = async (data: FieldValues) => {
-    // TODO: submit to server
-    // ....
+  const signupMutation = useMutation(trpc.v1.user.signup.mutationOptions());
 
-    reset;
+  const onSubmit = (data: SignUpSchema) => {
+    signupMutation.mutate(data, {
+      onSuccess: () => {
+        reset();
+      },
+    });
   };
 
   return (
@@ -45,12 +51,15 @@ export default function SignUp() {
           <p className="text-red-500">{`${errors.password.message}`}</p>
         )}
         <button
-          disabled={isSubmitting}
+          disabled={signupMutation.isPending}
           type="submit"
           className="cursor-pointer rounded bg-emerald-400 py-2 text-neutral-900 disabled:bg-gray-300"
         >
           Submit
         </button>
+        {signupMutation.error && (
+          <p className="text-red-500">{signupMutation.error.message}</p>
+        )}
       </form>
     </div>
   );
