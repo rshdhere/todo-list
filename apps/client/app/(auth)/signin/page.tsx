bf634b7fca44a59431813f572d6cda0235a75980
raashed <mohammedraashed10@gmail.com>
2026-03-17 17:36:54 +0530

    feat(signin): initialized /signin page using legacy-html based forms

--- apps/client/app/(auth)/signin/page.tsx
+++ apps/client/app/(auth)/signin/page.tsx
@@ -1,3 +1,16 @@
 export default function SignIn() {
-  return <div>SignIn Page</div>;
+  return (
+    <form className="flex flex-col gap-y-2">
+      <input
+        type="email"
+        placeholder="email"
+        className="rounded px-4 py-2.5 outline-none selection:bg-emerald-300"
+      />
+      <input
+        type="password"
+        placeholder="password"
+        className="rounded px-4 py-2.5 outline-none selection:bg-emerald-300"
+      />
+    </form>
+  );
 }
