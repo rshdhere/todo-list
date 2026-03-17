addb050781001cb929cfef59c451c1bb858a5eee
raashed <mohammedraashed10@gmail.com>
2026-03-17 17:51:00 +0530

    feat(hookform): added zod resolver from hookform

--- apps/client/app/(auth)/signin/page.tsx
+++ apps/client/app/(auth)/signin/page.tsx
@@ -1,12 +1,28 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { authSchema } from "@todo-list/validators";
+
 export default function SignIn() {
+  const { register, handleSubmit } = useForm({
+    resolver: zodResolver(authSchema.input),
+  });
+
+  const onSubmit = () => {
+    // TODO : send it to server
+  };
+
   return (
-    <form className="flex flex-col gap-y-2">
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-y-2">
       <input
+        {...register("email")}
         type="email"
         placeholder="email"
         className="rounded px-4 py-2.5 outline-none selection:bg-emerald-300"
       />
       <input
+        {...register("password")}
         type="password"
         placeholder="password"
         className="rounded px-4 py-2.5 outline-none selection:bg-emerald-300"
