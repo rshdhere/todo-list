5f3d58db6867a6b66a957e9adf5a98ed22fe8e5f
raashed <mohammedraashed10@gmail.com>
2026-03-19 06:10:39 +0530

    refactor(validation): added ResendVerification Schema for managing /api

--- packages/validators/src/index.ts
+++ packages/validators/src/index.ts
@@ -7,3 +7,6 @@ export * from "@todo-list/validators/todo";
 export type AuthSchema = z.infer<typeof authSchema.input>;
 export type SignUpSchema = AuthSchema;
 export type SignInSchema = AuthSchema;
+export type ResendVerificationSchema = z.infer<
+  typeof authSchema.resendVerificationInput
+>;
